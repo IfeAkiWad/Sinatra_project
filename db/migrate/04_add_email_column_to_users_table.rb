cc645723d95d86e04c3decd69251abbec07181e8
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-07-27 16:30:11 -0500

    updated to 04_add_email_column_to_users_table.rb

--- db/migrate/04_add_email_column_to_users_table.rb
+++ db/migrate/04_add_email_column_to_users_table.rb
@@ -1,4 +1,4 @@
-class AddColumnToUsersTable < ActiveRecord::Migration
+class AddEmailColumnToUsersTable < ActiveRecord::Migration
     def change
         add_column :users, :email, :string
     end
