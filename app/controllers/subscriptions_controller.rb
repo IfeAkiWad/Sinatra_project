52e79ec033e9ad7ee281877d66f69f751bd366af
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-08-01 14:50:27 -0500

    Attempt to fix syntax error

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,23 +1,23 @@
 class SubscriptionsController < ApplicationController
     #index action
- get '/subscriptions' do
-    if logged_in?
-        @subscriptions = current_user.subscriptions
-        erb :'subscriptions/index'
-    else
-        redirect '/sessions/login'
-    end
- end
+   get '/subscriptions' do
+      if logged_in?
+         @subscriptions = current_user.subscriptions
+         erb :'subscriptions/index'
+      else
+         redirect '/sessions/login'
+      end
+   end
 
- get '/products/:product_id/subscriptions/new' do 
-    @product = Product.find_by_id(params[:product_id])
-    erb :'subscriptions/new'
- end
+   get '/products/:product_id/subscriptions/new' do 
+      @product = Product.find_by_id(params[:product_id])
+      erb :'subscriptions/new'
+   end
 
- post '/subscriptions' do
+   post '/subscriptions' do
     #because of the hidden field, params will now have a key/value pair called product_id
     #calling current_user.subscriptions.create(params) will the associate that new subscription with that product and user
-    @subscription = current_user.subscriptions.create(params)
- end
+      @subscription = current_user.subscriptions.create(params)
+   end
 
 end
